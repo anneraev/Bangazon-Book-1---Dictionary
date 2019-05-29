e47bfd20a203729aca50e7cda3c19c38af154ccd
anneraev <47757104+anneraev@users.noreply.github.com>
2019-05-29 15:04:14 -0500

    finished chapter 5 practices

--- Program.cs
+++ Program.cs
@@ -71,6 +71,35 @@ namespace dictionary
                 }
                 Console.WriteLine("___________________");
             }
+
+            //idioms exercise
+            Dictionary<string, List<string>> idioms = new Dictionary<string, List<string>>();
+            idioms.Add("Penny", new List<string> { "A", "penny", "for", "your", "thoughts" });
+            idioms.Add("Injury", new List<string> { "Add", "insult", "to", "injury" });
+            idioms.Add("Moon", new List<string> { "Once", "in", "a", "blue", "moon" });
+            idioms.Add("Grape", new List<string> { "I", "heard", "it", "through", "the", "grapevine" });
+            idioms.Add("Murder", new List<string> { "Kill", "two", "birds", "with", "one", "stone" });
+            idioms.Add("Limbs", new List<string> { "It", "costs", "an", "arm", "and", "a", "leg" });
+            idioms.Add("Grain", new List<string> { "Take", "what", "someone", "says", "with", "a", "grain", "of", "salt" });
+            idioms.Add("Fences", new List<string> { "I'm", "on", "the", "fence", "about", "it" });
+            idioms.Add("Sheep", new List<string> { "Pulled", "the", "wool", "over", "his", "eyes" });
+            idioms.Add("Lucifer", new List<string> { "Speak", "of", "the", "devil" });
+
+            foreach(KeyValuePair<string, List<string>> wordList in idioms){
+                string sentence = "";
+                foreach(String word in wordList.Value) {
+                    sentence += word;
+                    int index = wordList.Value.IndexOf(word);
+                    int length = wordList.Value.Count;
+                    length -= 1;
+                    if (index != length) {
+                        sentence += " ";
+                    }
+                }
+                sentence += ".";
+                Console.WriteLine(sentence);
+                Console.WriteLine("___________________");
+            }
         }
     }
 }
